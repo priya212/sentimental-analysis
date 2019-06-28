197f91b6fb525302707a6a8a088bfb399864fcca
priyanka <darekarpriyanka2@gmail.com>
2019-06-28 15:45:40 +0530

    sentimental analysis

---
+++ src/main/java/com/stackroute/sentimentalanalysis/service/ReviewerService.java
@@ -0,0 +1,15 @@
+package com.stackroute.sentimentalanalysis.service;
+
+import com.stackroute.sentimentalanalysis.domain.Reviewer;
+
+import java.util.List;
+
+public interface ReviewerService {
+    public Reviewer saveReviewer(Reviewer reviewer) ;
+    public List<Reviewer> getAllReviewer();
+   // public Reviewer getReview(String reviewerName);
+   // public Reviewer getReviewerByName(String reviewerName);
+    public Reviewer deleteReviewer(String reviewerName);
+    public int findSentiment(Reviewer reviewer);
+    public String sentimentResult(int sentiments);
+}
---
+++ src/main/java/com/stackroute/sentimentalanalysis/service/ReviewerServiceImpl.java
@@ -0,0 +1,140 @@
+package com.stackroute.sentimentalanalysis.service;
+
+import com.stackroute.sentimentalanalysis.domain.Reviewer;
+import com.stackroute.sentimentalanalysis.repository.ReviewerRepository;
+import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.SentenceAnnotator;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Properties;
+
+@Service
+public class ReviewerServiceImpl implements ReviewerService{
+
+    private ReviewerRepository reviewerRepository;
+    private StanfordCoreNLP pipeline;
+    Properties properties=new Properties();
+
+    @PostConstruct
+    public void init(){
+        properties.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,parse,natlog,openie,sentiment");
+       /* properties.setProperty("pos.model", "russian-ud-pos.tagger");
+        properties.setProperty("ner.model", "english_UD.gz");
+        properties.setProperty("ner.useSUTime", "false");
+        properties.setProperty("ner.applyNumericClassifiers", "false");
+        properties.setProperty("sutime.includeRange", "false");*/
+        pipeline=new StanfordCoreNLP(properties);
+    }
+
+    @Autowired
+    public ReviewerServiceImpl(ReviewerRepository reviewerRepository) {
+        this.reviewerRepository = reviewerRepository;
+    }
+
+    @Override
+    public Reviewer saveReviewer(Reviewer reviewer) {
+        reviewerRepository.save(reviewer);
+        return reviewer;
+    }
+
+    @Override
+    public List<Reviewer> getAllReviewer() {
+        List<Reviewer> reviewerList=reviewerRepository.findAll();
+        return reviewerList;
+    }
+
+    @Override
+    public Reviewer deleteReviewer(String reviewerName) {
+        reviewerRepository.deleteById(reviewerName);
+        return null;
+    }
+
+    @Override
+    public int findSentiment(Reviewer reviewer) {
+        int mainSentiment=0;
+        String clearReviewText=clearReview(reviewer.getReview());
+        if(clearReviewText != null && clearReviewText.length()>0)
+        {
+            int longest=0;
+            Annotation annotation=pipeline.process(clearReviewText);
+            for(CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class))
+            {
+                Tree tree=sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+                int sentiment= RNNCoreAnnotations.getPredictedClass(tree);
+                String parseText=sentence.toString();
+                if(parseText.length()>longest){
+                    mainSentiment=sentiment;
+                    longest=parseText.length();
+                }
+            }
+        }
+        return mainSentiment;
+    }
+
+    @Override
+    public String sentimentResult(int sentiments) {
+        String result="";
+        if(sentiments == 0)
+        {
+            result="Very negative";
+        }
+        else if(sentiments == 1)
+        {
+            result ="Negative";
+        }
+        else  if(sentiments == 2)
+        {
+            result="Neutral";
+        }
+        else if(sentiments == 3)
+        {
+            result="Positive";
+        }
+        else {
+            result="Very Positive";
+        }
+        return  result;
+    }
+
+    public String clearReview(String review)
+    {
+        review=review.toLowerCase();
+        return  review;
+    }
+
+    //
+//    @Override
+//    public Reviewer getReviewerByName(String reviewerName) {
+//        Reviewer reviewer=null;
+//        if(reviewerRepository.existsById(reviewerName)) {
+//            reviewer=;
+//        }
+//        return  reviewer;
+//    }
+
+
+//    @Override
+//    public Reviewer getReview(String reviewerName) {
+//        List<Reviewer> reviewerList=getAllReviewer();
+//        Reviewer reviewer=null;
+//        String review="";
+//        if(reviewerList.contains(reviewerName))
+//        {
+//             reviewer=reviewerList.get(0);
+//             //review=reviewer.getReview();
+//        }
+//
+//        return reviewerRepository.getReviewerByName(reviewerName);
+//    }
+
+}
